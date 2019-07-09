485d3f1abc615ab4cdd7e47ceb062ed175ede410
Danielly Queiroz <danielly.borba.moura@accenture.com>
2019-07-09 16:30:45 -0300

    OCR implementado em Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +25,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Camera,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
   ]
 })
-export class AppModule {}
+export class AppModule { }
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,99 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
+import * as Tesseract from 'tesseract.js'
+import { Camera, PictureSourceType } from '@ionic-native/camera';
+import { LoadingController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedImage: string;
+  imageText: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera, private actionSheetCtrl: ActionSheetController, public loadingCtrl: LoadingController,public toastCtrl: ToastController) {
 
   }
+  selectSource() {
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
+  presentLoading() {
+    return this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+  }
+
+  recognizeImage() {
+    const { TesseractWorker, OEM } = Tesseract;
+
+    this.presentLoading().present();
+    Tesseract.recognize(this.selectedImage,'por',{
+     'tessedit_char_whitelist': '0123456789ABCDEFGHIJLMNOPQRSTUVXWZ',
+    })
+      .progress(message => {
+        console.log(message.status);
+
+      })
+      .catch(err => console.error(err))
+      .then(result => {
+        console.log("entrou aqui");
+        this.imageText = result.text;
+        if(this.imageText ==""){
+          const toast = this.toastCtrl.create({
+            message: 'O algortimo não conseguiu identificar o texto, tire uma imagem melhor',
+            duration: 3000
+          });
+          toast.present();
+
+        }
+        this.presentLoading().dismiss();
+
+      })
+      .finally(resultOrError => {
+        // this.progress.complete();
+        console.log(resultOrError);
+        this.presentLoading().dismiss();
+
+
+      });
+  }
+
 
 }
+
